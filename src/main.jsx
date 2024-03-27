1868c55915940b4a4376873240c901d54a8138ab
MohammadSobhi <129908365+MohammadSobhi@users.noreply.github.com>
2024-03-28 00:25:51 +0200

    deleted App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import User from './pages/User'
+import Cart from './pages/Cart'
+import Layout from './components/Layout'
+import ProductDetails from './pages/ProductDetails'
+import ErrorPage from './pages/ErrorPage'
+import Login from './pages/Login'
+import AuthRequired from './components/AuthRequired' 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Layout/> } >
+          <Route index element={ <Home/> } />
+          <Route path='about' element={ <About/> } />
+          <Route element={<AuthRequired/>}>
+            <Route path='user' element={ <User/> } />
+            <Route path='cart' element={ <Cart/> } />
+          </Route>  
+          <Route path='products/:id' element={ <ProductDetails/> } />
+          <Route path='login' element={ <Login/> } />
+          <Route path='*' element={ <ErrorPage/> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
